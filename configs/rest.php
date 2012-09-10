2be3149a7776bd83ffe58d2661d5571a23f00b8d
Trevor Suarez <rican7@gmail.com>
2012-09-10 12:25:12 -0400

    Added more default status codes and expanded the sample responses.

--- configs/rest.php
+++ configs/rest.php
@@ -10,7 +10,17 @@ $load_config = function() {
 		),
 		'status-codes' => array(
 			200 => 'OK',
+			400 => 'BAD_REQUEST',
+			401 => 'UNAUTHORIZED',
+			403 => 'FORBIDDEN',
 			404 => 'NOT_FOUND',
+			405 => 'METHOD_NOT_ALLOWED',
+			406 => 'NOT_ACCEPTABLE',
+			429 => 'RATE_LIMITED',
+			500 => 'INTERNAL_SERVER_ERROR',
+			502 => 'BAD_GATEWAY',
+			503 => 'SERVICE_UNAVAILABLE',
+			504 => 'GATEWAY_TIMEOUT',
 		),
 		'mime-types' => array(
 			'json' => 'application/json',
--- routes/sample.php
+++ routes/sample.php
@@ -1,10 +1,38 @@
 <?php
 
 // Base response (Make the trailing splash optional to catch accidents and laziness)
-respond( '/?', function( $request, $response, $app ) {
-	// Show that we have access to the config
-	//var_dump( $app->config );
-	
+respond( '/[a:key]?', function( $request, $response, $app ) {
+	// Get our sent parameter
+	$key_to_encrypt = $request->key;
+
+	// Show some sample data
+	$response->data = Api::get_sample_data( $key_to_encrypt );
+});
+
+// Get a sample of user data
+respond( 'POST', '/user/?', function( $request, $response, $app ) {
+	// Get our sent parameter
+	$user_id = $request->param( 'id' );
+
+	// Did we get our required params?
+	if ( is_null( $user_id ) ) {
+		$response->abort( 400, null, 'A requested user id was not sent' );
+	}
+
+	// Let's get the "user's profile"
+	$sample_profile = Api::get_sample_user_data( $user_id );
+
+	if ( !is_null($sample_profile) ) {
+		// Show some data for testing
+		$response->data = $sample_profile;
+	}
+	else {
+		$response->abort( 404, null, 'A user with that id was not found.' );
+	}
+});
+
+// Get a sample of database data
+respond( '/db/?', function( $request, $response, $app ) {
 	// Show some data to test our DB connection
-	$response->data = Api::get_sample_data();
+	$response->data = Api::get_sample_db_data();
 });
