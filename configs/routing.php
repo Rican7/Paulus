e1fe6a9520b6c7a8b94b93a3abb45bbc0b3176f7
Trevor Suarez <rican7@gmail.com>
2012-10-09 18:58:04 -0400

    Set automatic route loading to false by default

--- configs/routing.php
+++ configs/routing.php
@@ -3,7 +3,7 @@
 // Define all of our routes (we could do this automatically, but we'd like more control)
 $load_config = function() {
 	return array(
-		'load_all_automatically' => true,
+		'load_all_automatically' => false,
 		'routes' => array(
 			'sample',
 		),
