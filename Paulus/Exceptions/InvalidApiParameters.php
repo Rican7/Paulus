065b5d2af0567c54e43e87449a0c9aa167e91996
Trevor Suarez <rican7@gmail.com>
2013-02-07 03:06:45 -0500

    Now using new "traits" to remove repeated code

--- Paulus/Exceptions/InvalidApiParameters.php
+++ Paulus/Exceptions/InvalidApiParameters.php
@@ -3,24 +3,17 @@
 namespace Paulus\Exceptions;
 
 use	\Paulus\Exceptions\Interfaces\ApiException,
+	\Paulus\Exceptions\Traits\ApiExceptionBase,
 	\InvalidArgumentException;
 
 // InvalidApiParameters Exception 
 class InvalidApiParameters extends InvalidArgumentException implements ApiException {
+	// Use trait
+	use ApiExceptionBase;
 
 	// Define unique properties
 	protected $code = 400;
 	protected $slug = 'INVALID_API_PARAMETERS';
 	protected $message = 'The posted data did not pass validation';
-
-	// Return the slug
-	public function getSlug() {
-		return $this->slug;
-	}
-
-	// Alias of getSlug
-	public function get_slug() {
-		return $this->getSlug();
-	}
 
 } // End class InvalidApiParameters
--- Paulus/Exceptions/WrongMethod.php
+++ Paulus/Exceptions/WrongMethod.php
@@ -4,45 +4,18 @@ namespace Paulus\Exceptions;
 
 use	\Paulus\Exceptions\Interfaces\ApiException,
 	\Paulus\Exceptions\Interfaces\ApiVerboseException,
+	\Paulus\Exceptions\Traits\ApiVerboseExceptionBase,
 	\OutOfBoundsException;
 
 // WrongMethod Exception 
 class WrongMethod extends OutOfBoundsException implements ApiVerboseException {
+	// Use trait
+	use ApiVerboseExceptionBase;
 
 	// Define unique properties
 	protected $code = 405;
 	protected $slug = 'METHOD_NOT_ALLOWED';
 	protected $message = 'The wrong method was called on this endpoint';
 	protected $more_info = array();
-
-	// Return the slug
-	public function getSlug() {
-		return $this->slug;
-	}
-
-	// Return the more_info
-	public function getMoreInfo() {
-		return $this->more_info;
-	}
-
-	// Set the more_info
-	public function setMoreInfo( array $more_info ) {
-		return $this->more_info = $more_info;
-	}
-
-	// Alias of getSlug
-	public function get_slug() {
-		return $this->getSlug();
-	}
-
-	// Alias of getMoreInfo
-	public function get_more_info() {
-		return $this->getMoreInfo();
-	}
-
-	// Alias of setMoreInfo
-	public function set_more_info( array $more_info ) {
-		return $this->setMoreInfo( $more_info );
-	}
 
 } // End class WrongMethod
