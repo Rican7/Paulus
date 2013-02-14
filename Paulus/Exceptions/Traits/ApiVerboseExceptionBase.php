acecb4a39255ab6779d10bdf95d2896f0e3d300d
Trevor Suarez <rican7@gmail.com>
2013-02-14 03:20:59 -0500

    Adding the new Paulus file header on all of the Exception namespaced Paulus classes

--- Paulus/Exceptions/Traits/ApiVerboseExceptionBase.php
+++ Paulus/Exceptions/Traits/ApiVerboseExceptionBase.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Paulus - A PHP micro-framework for creating RESTful API services
+ *
+ * @author		Trevor Suarez (Rican7)
+ * @copyright	2013 Trevor Suarez
+ * @link		https://github.com/Rican7/Paulus/
+ * @license		https://github.com/Rican7/Paulus/blob/master/LICENSE
+ * @version		0.10.0
+ */
 
 namespace Paulus\Exceptions\Traits;
 
