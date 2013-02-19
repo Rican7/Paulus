0db4a1b9abb59cee832f4feafdd09eac497d990a
Trevor Suarez <rican7@gmail.com>
2013-02-19 01:47:18 -0500

    Now setting the routing library location in the routing config Also, removed hard-coded routes that have been deleted

--- configs/routing.php
+++ configs/routing.php
@@ -3,10 +3,10 @@
 // Define all of our routes (we could do this automatically, but we'd like more control)
 $load_config = function() {
 	return array(
+		'routing_library_location' => PAULUS_EXTERNAL_LIB_DIR . '/klein/klein.php',
 		'load_all_automatically' => false,
 		'routes' => array(
-			'index',
-			'sample',
+			// 'index',
 		),
 		'top_level_route' => 'index',
 		'auto_start_controllers' => true,
