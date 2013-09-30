97439fd294cccc8646ab163a426dc419f2059f00
Trevor Suarez <rican7@gmail.com>
2013-09-30 16:35:47 -0400

    PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,27 +1,14 @@
 <?php
+/**
+ * Paulus - A PHP micro-framework for creating RESTful services
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2013 Trevor Suarez
+ * @link        https://github.com/Rican7/Paulus
+ * @license     https://github.com/Rican7/Paulus/blob/master/LICENSE
+ * @version     2.0.0
+ */
+
 // Load our autoloader, and add our Test class namespace
-$autoloader = require( __DIR__ . '/../vendor/autoload.php' );
-$autoloader->add( 'Paulus\Tests', __DIR__ );
-
-// Setup our test configuration
-$paulus_config = array(
-    'database' => array(
-        'model_directory' => '',
-    ),
-    'routing' => array(
-        'load_all_automatically' => false,
-    ),
-	'rest' => array(
-		'exit_after_rest_response' => false,
-	),
-);
-
-
-// Overwrite Klein's "_Headers" class for PHPUnit testing
-// (PHPUnit doesn't like header output...)
-class HeadersEcho extends _Headers {
-    public function header($key, $value = null) {
-        //echo $this->_header($key, $value) . "\n";
-    }
-}
-_Request::$_headers = _Response::$_headers = new HeadersEcho;
+$autoloader = require(__DIR__ . '/../vendor/autoload.php');
+$autoloader->add('Paulus\Tests', __DIR__);
