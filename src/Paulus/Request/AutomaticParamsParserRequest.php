6f1fe6ca4d23b5d33003a6363ffbdf139e6816a0
Trevor Suarez <rican7@gmail.com>
2013-10-08 12:50:19 -0400

    New request class for automatically parsing encoded request parameters beyond PHP's default behavior

---
+++ src/Paulus/Request/AutomaticParamsParserRequest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Paulus - A PHP micro-framework for creating RESTful services
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2013 Trevor Suarez
+ * @link        https://github.com/Rican7/Paulus
+ * @license     https://github.com/Rican7/Paulus/blob/master/LICENSE
+ * @version     2.0.0
+ */
+
+namespace Paulus\Request;
+
+use Paulus\Exception\Http\InvalidRequestSyntax;
+
+/**
+ * AutomaticParamsParserRequest
+ *
+ * @uses    Request
+ * @package Paulus\Request
+ */
+class AutomaticParamsParserRequest extends Request
+{
+
+    /**
+     * Create a new request from the PHP request globals
+     *
+     * @see Klein\Request::createFromGlobals()
+     * @static
+     * @access public
+     * @return AutomaticParamsParserRequest
+     */
+    public static function createFromGlobals()
+    {
+        // First, get our object from our parent handler
+        $request = parent::createFromGlobals();
+
+        // Should we even be handling params?
+        if ($request->method('POST')
+            || $request->method('PUT')
+            || $request->method('PATCH')
+            || $request->method('DELETE')) {
+
+            // Parse Form URL Encoded attributes
+            if (strpos($request->headers()->get('content-type'), 'application/x-www-form-urlencoded') !== false
+                && !$request->method('POST')) {
+
+                // Parse the body into a params array
+                parse_str($request->body(), $params);
+
+                // Replace the post params with the parse params
+                $request->paramsPost()->replace($params);
+
+            } elseif (strpos($request->headers()->get('content-type'), 'application/json') !== false
+                || strpos($request->headers()->get('content-type'), 'application/x-json') !== false) {
+
+                // Decode the JSON body
+                $params = json_decode($request->body(), true);
+
+                // If there were no decoding errors
+                if (JSON_ERROR_NONE === json_last_error()) {
+                    // Replace the post params with the parse params
+                    $request->paramsPost()->replace($params);
+
+                } else {
+                    // Must have been a decoding error
+                    throw new InvalidRequestSyntax();
+                }
+            }
+        }
+
+        return $request;
+    }
+}
---
+++ src/Paulus/Request/Request.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Paulus - A PHP micro-framework for creating RESTful services
+ *
+ * @author      Trevor Suarez (Rican7)
+ * @copyright   2013 Trevor Suarez
+ * @link        https://github.com/Rican7/Paulus
+ * @license     https://github.com/Rican7/Paulus/blob/master/LICENSE
+ * @version     2.0.0
+ */
+
+namespace Paulus\Request;
+
+use Klein\Request as KleinRequest;
+
+/**
+ * Request
+ *
+ * @uses    KleinRequest
+ * @package Paulus\Request
+ */
+class Request extends KleinRequest
+{
+}
